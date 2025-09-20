72cfef0ca4900512be5ba65693cecaebe2957860
Ashish singotra <ashishk6621@gmail.com>
2025-09-20 18:43:44 +0530

    Auth and api-gateway changes

---
+++ auth-service/src/main/java/com/pm/auth_service/service/AuthService.java
@@ -0,0 +1,51 @@
+package com.pm.auth_service.service;
+
+import com.pm.auth_service.dto.LoginRequestDTO;
+import com.pm.auth_service.util.JwtUtil;
+import io.jsonwebtoken.JwtException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthService {
+
+    private static final Logger log = LoggerFactory.getLogger(AuthService.class);
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+
+    @Autowired
+    public AuthService(UserService userService,
+                       PasswordEncoder passwordEncoder,
+                       JwtUtil jwtUtil) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+    }
+
+    public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
+        Optional<String> token = userService
+                .findByEmail(loginRequestDTO.getEmail())
+                .filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword(),
+                        u.getPassword()))
+                .map(u -> jwtUtil.generateToken(u.getEmail(), u.getRole()));
+
+        return token;
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            log.info("Token: {}",token);
+            jwtUtil.validateToken(token);
+            return true;
+        } catch (JwtException e) {
+            log.error("User Authentication failed : {}",e.getMessage());
+            return false;
+        }
+    }
+}
---
+++ auth-service/src/main/java/com/pm/auth_service/service/UserService.java
@@ -0,0 +1,24 @@
+package com.pm.auth_service.service;
+
+import com.pm.auth_service.model.User;
+import com.pm.auth_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public Optional<User> findByEmail(String email){
+
+        return userRepository.findByEmail(email);
+    }
+}
