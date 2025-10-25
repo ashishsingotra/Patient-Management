b62490c729e865b0d50f774460060998fb4a6312
Ashish singotra <ashishk6621@gmail.com>
2025-10-25 20:48:15 +0530

    api-gateway and integration testing

--- auth-service/src/main/java/com/pm/auth_service/util/JwtUtil.java
+++ auth-service/src/main/java/com/pm/auth_service/util/JwtUtil.java
@@ -18,30 +18,30 @@ public class JwtUtil {
 
     private final Key secretKey;
 
-    public JwtUtil(@Value("${Jwt.secret}") String secret){
-        byte[] keyBytes = Base64.getDecoder().decode(secret.
-                getBytes(StandardCharsets.UTF_8));
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
+        byte[] keyBytes = Base64.getDecoder()
+                .decode(secret.getBytes(StandardCharsets.UTF_8));
         this.secretKey = Keys.hmacShaKeyFor(keyBytes);
     }
 
-    public String generateToken(String email, String role){
+    public String generateToken(String email, String role) {
         return Jwts.builder()
                 .subject(email)
-                .claim("role",role)
+                .claim("role", role)
                 .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 *10)) //10 hours
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours
                 .signWith(secretKey)
                 .compact();
     }
 
-    public void validateToken(String token){
+    public void validateToken(String token) {
         try {
             Jwts.parser().verifyWith((SecretKey) secretKey)
                     .build()
-                    .parseEncryptedClaims(token);
-        } catch (SignatureException e){
+                    .parseSignedClaims(token);
+        } catch (SignatureException e) {
             throw new JwtException("Invalid JWT signature");
-        } catch (JwtException e){
+        } catch (JwtException e) {
             throw new JwtException("Invalid JWT");
         }
     }
